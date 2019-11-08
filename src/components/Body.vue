0f4967d2d6ff3b9580292513533128f26ce0634f
Sabino Velasquez <sabinovelasquez@gmail.com>
2019-11-08 19:37:16 -0300

    feat: header structure, body changes

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,6 +1,22 @@
 <template lang='pug'>
-  section
-    p Body
+.cont
+  section.us
+    .container
+      .md-layout.md-alignment-center-center
+        .md-layout-item.md-small-size-100.us-av
+          img(src='@/assets/img/us-av.png', alt='Us')
+        .md-layout-item.md-small-size-100
+          h2.title Sobre Nosotros
+          p Contamos con la confianza y compromiso de nuestros productores y proveedores orientado a satisfacer los requerimientos y exigencias de cada uno de nuestros mercados.
+          p Nuestro principal objetivo es entregar a nuestros clientes, con quienes hemos desarrollado importantes lazos comerciales, un servicio cordial y personalizado. Toda nuestra fruta cumple con las certificaciones y protocolos exigidos internacionalmente.
+          p Apostamos por la apertura de nuevos mercados para nuestra fruta y seguir impulsando nuestro continuo crecimiento.
+  section.catalog
+    .container
+      .md-layout.md-alignment-center-center
+        .md-layout-item
+          h2.title Nuestros productos
+      .md-layout.md-alignment-center-center
+        p Under construction...
 </template>
 
 <script>
@@ -10,4 +26,40 @@ export default {
 </script>
 
 <style scoped lang='scss'>
+@import '@/assets/global.scss';
+
+.us-av{
+  img{
+    margin:auto;
+    display: flex;
+  }
+}
+section{
+  padding: 30px 0 60px;
+  h2.title{
+    margin: 0;
+    @media (max-width: 960px){
+      margin-top: 30px;
+    }
+  }
+  &.us{
+    background-color: $dark-grey;
+    h2.title{
+      color: $primary-color;
+      font-size: 50px;
+      font-weight: 100;
+      line-height: 50px;
+    }
+  }
+  &.catalog{
+    background-color: #fbfbfb;
+    color: $dark-grey;
+    h2.title{
+      
+      text-align: center;
+      font-weight: 100;
+      line-height: 50px;
+    }
+  }
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,20 @@
 <template lang='pug'>
   section
     header.bg
-      .shadow
       .container.md-layout.md-alignment-center
-        .md-layout-item
-          h1 Item
+        .logo
+          img.logo-cherry(svg-inline, src='@/assets/img/cherry-logo.svg', alt='Southernlands')
+          img.logo-text(svg-inline, src='@/assets/img/southernlands-logo-text.svg', alt='Southernlands')
+      .container.md-layout.md-alignment-center
+        p.tagline Exportadores de fruta para Asia, Emiratos Árabes y el resto del mundo.
     .nav-bar
       .container
-        md-menu(md-direction='bottom-start')
+        md-menu.menu(md-size='small', md-direction='bottom-start')
+          md-button.md-icon-button(md-menu-trigger)
+            img.logo-menu(svg-inline, src='@/assets/img/cherry-logo.svg')
           md-button(md-menu-trigger) Nosotros
           md-button(md-menu-trigger) Catálogo
+          md-button(md-menu-trigger) Contacto
 </template>
 
 <script>
@@ -21,16 +26,36 @@ export default {
 <style scoped lang='scss'>
 .nav-bar{
   background-color: #1A1A1A;
+  .menu{
+    .md-button{
+      margin-left: 8px;
+    }
+  }
 }
 header.bg{
-  min-height: 360px;
-  background: linear-gradient(
-    rgba(0, 0, 0, 1), 
-  rgba(0, 0, 0, 0)
-  ),
-  url('../assets/img/bg-1.jpg');
+  height: 360px;
+  background: linear-gradient(to bottom, rgba(0,0,0,.9), rgba(0,0,0,0) 360px), url('../assets/img/bg-1.jpg');
   background-size: cover;
-  background-position: center;
+  background-position: center center;
+  .logo {
+    margin: 80px auto 0;
+    width: 120px;
+    text-align: center;
+    .logo-cherry{
+      width: 40px;
+    }
+    .logo-text{
+      margin-top: 8px;
+      width: 100%;
+    }
+  }
+  .tagline{
+    text-shadow: -1px 1px 0px rgba(0, 0, 0, 1);
+  }
 }
-
+img.logo-menu{
+    width: 18px;
+    margin-left: -3px;
+    margin-bottom: 9px;
+  }
 </style>
