5df76d60d3986c300899b0c7dda3ba6abe3c1d24
Sabino Velasquez <sabinovelasquez@gmail.com>
2019-11-08 15:05:26 -0300

    header structure

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,15 @@
 <template lang='pug'>
-  header
-    h1 Test CI
+  section
+    header.bg
+      .shadow
+      .container.md-layout.md-alignment-center
+        .md-layout-item
+          h1 Item
+    .nav-bar
+      .container
+        md-menu(md-direction='bottom-start')
+          md-button(md-menu-trigger) Nosotros
+          md-button(md-menu-trigger) Catálogo
 </template>
 
 <script>
@@ -9,5 +18,19 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
+<style scoped lang='scss'>
+.nav-bar{
+  background-color: #1A1A1A;
+}
+header.bg{
+  min-height: 360px;
+  background: linear-gradient(
+    rgba(0, 0, 0, 1), 
+  rgba(0, 0, 0, 0)
+  ),
+  url('../assets/img/bg-1.jpg');
+  background-size: cover;
+  background-position: center;
+}
+
 </style>
