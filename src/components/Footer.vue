e0d45cc0e5ae5e267c9c6e1e42fd6d93402202b4
Sabino Velasquez <sabinovelasquez@gmail.com>
2019-11-12 13:31:52 -0300

    feat: data binding

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,12 @@
 <template lang='pug'>
   footer
-    .container.md-layout.md-alignment-center
+    .container.md-layout.md-alignment-center(v-if='!loading')
       .md-layout-item.md-small-size-100
-        h4 Southernlands Chile
-        p Padre Mariano #291
-          br
-          | Providencia, Santiago.
+        h4 {{footer.address[lang].title}}
+        p(v-for='item in footer.address[lang].content') {{item}}
         p
           a(href='mailto:marcela@southernlands.cl') info@southernlands.cl
-      .md-layout-item.md-small-size-100(v-if='footer.certification')
+      .md-layout-item.md-small-size-100
         h4 {{footer.certification[lang].title}}
         ul
           li(v-for='item in footer.certification[lang].content') {{item}}
@@ -24,14 +22,22 @@ export default {
   name: 'Footer',
   data() {
     return {
+      loading: true,
+      error: null,
       footer: {}
     }
   },
   computed: {
     ...mapGetters('lang', ['lang'])
   },
-  firestore: {
-    footer: db.collection('general').doc('footer')
+  mounted() {
+    this.$bind('footer', db.collection('general').doc('footer'))
+      .then( () => {
+        this.loading = false
+      })
+      .catch((error) => {
+        this.error = error
+      })
   }
 }
 </script>
